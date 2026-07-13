4cd72bff5281866f1fa944aef7916d6866ce2b91
Aly Smith <63546997+alydevs@users.noreply.github.com>
2026-07-13 19:55:08 +1000

    Update Z0146SouthernThanalan.cs

--- vnavmesh/Customizations/Z0146SouthernThanalan.cs
+++ vnavmesh/Customizations/Z0146SouthernThanalan.cs
@@ -1,4 +1,6 @@
-﻿namespace Navmesh.Customizations;
+﻿using System.Collections.Generic;
+
+namespace Navmesh.Customizations;
 
 [CustomizationTerritory(146)]
 internal class Z0146SouthernThanalan : NavmeshCustomization
@@ -14,5 +16,12 @@ internal class Z0146SouthernThanalan : NavmeshCustomization
         if (scene.Meshes.TryGetValue("bg/ffxiv/wil_w1/fld/w1f4/collision/tr1610.pcb", out var mesh2))
             foreach (var inst in mesh2.Instances)
                 inst.ForceClearPrimFlags |= SceneExtractor.PrimitiveFlags.Unlandable;
+    }
+    
+    public override void CustomizeMesh(Navmesh mesh, List<uint> festivalLayers)
+    {
+        // lower bridge to ruined stone structure
+        LinkPoints(mesh, new(-62.317863f, -16.855253f, -172.60663f), new(-64.9727f, -15.593737f, -171.45671f), Navmesh.AreaId.Default);
+        LinkPoints(mesh, new(-64.9727f, -15.593737f, -171.45671f), new(-62.317863f, -16.855253f, -172.60663f), Navmesh.AreaId.Default);
     }
 }
